8aad9752834b3fc44fd341dd8457cac4ab2bc5c9
RichardZhangCS <richardzhang1303@gmail.com>
2021-12-17 00:27:39 -0500

    Removed cors

--- server/app.js
+++ server/app.js
@@ -4,7 +4,6 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-var cors = require("cors");
 require("dotenv").config();
 
 var shortenerRouter = require("./routes/urlshortener");
@@ -12,12 +11,6 @@ var shortenerRouter = require("./routes/urlshortener");
 var app = express();
 
 // view engine setup
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    optionsSuccessStatus: 200,
-  })
-);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
