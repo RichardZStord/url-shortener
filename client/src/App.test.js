215e90b1e1f00688b94c9728dbb9fe8c3609778c
RichardZhangCS <richardzhang1303@gmail.com>
2021-12-18 13:27:35 -0500

    Fixed testing setup bugs

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import App from "./app";
+import App from "./App";
 
 test("URL Shortener Form renders without crashing", () => {
   render(<App />);
